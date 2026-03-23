7f59b4b9eda76af0d9bb1e5613a78a2f9515591c
Aravind <aravindrk651@gmail.com>
2026-03-23 10:55:00 +0530

    397 + Water Bottles

--- Java/Maths/397. Integer Replacement.java
+++ Java/Maths/397. Integer Replacement.java
@@ -7,7 +7,7 @@ If n is even, replace n with n / 2.
 If n is odd, replace n with either n + 1 or n - 1.
 Return the minimum number of operations needed for n to become 1.
 */
-
+/* Check out Collataz Conjecture */
 class Solution {
     public int integerReplacement(int n) {
         int ans=0;
