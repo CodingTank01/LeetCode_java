9edd4a89bcf439b26e06ef3f4a05a783896241ce
Aravind <aravindrk651@gmail.com>
2026-03-24 14:40:06 +0530

    file updates

---
+++ Java/Day-2/2481. Minimum Cuts to Divide a Circle.java
@@ -0,0 +1,21 @@
+/*
+2481. Minimum Cuts to Divide a Circle
+
+A valid cut in a circle can be:
+
+A cut that is represented by a straight line that touches two points on the edge of the circle and passes through its center, or
+A cut that is represented by a straight line that touches one point on the edge of the circle and its center.
+Some valid and invalid cuts are shown in the figures below.
+
+Given the integer n, return the minimum number of cuts needed to divide a circle into n equal slices.
+
+*/
+
+class Solution {
+    public int numberOfCuts(int n) {
+        if(n==1) return 0;
+
+        if(n%2==0) return n/2;
+        else return n;
+    }
+}
