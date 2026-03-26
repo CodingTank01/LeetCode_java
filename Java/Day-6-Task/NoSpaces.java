9a80bac4ff1546e3eea39f55439b126b7adbebda
Aravind <aravindrk651@gmail.com>
2026-03-26 07:37:11 +0530

    Day-6-Task

---
+++ Java/Day-6-Task/NoSpaces.java
@@ -0,0 +1,17 @@
+/* Remove all spaces from a string */
+
+import java.util.*;
+
+public class NoSpaces{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string value: ");
+        String str1 = sc.nextLine();
+        String out="";
+        for(int i=0; i<str1.length(); i++){
+            char ch = str1.charAt(i);
+            if(ch!=' ') out=out+ch;
+        }
+        System.out.print("Output: "+out+"\n");
+    }
+}
