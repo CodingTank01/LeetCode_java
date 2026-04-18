b96d77eea1e1fa3681fc7276804134e238d6f0e6
Aravind <aravindrk651@gmail.com>
2026-04-18 14:32:40 +0530

    Day21 Final Update

--- Java/Day-21/Main_Runnable1.java
+++ Java/Day-21/Main_Runnable1.java
@@ -18,18 +18,35 @@ class Work extends Pending implements Runnable {
         System.out.println("Thread 1 is running...");
     }
 }
+class Work2 implements Runnable {
+    Runnable r;
 
+    void Work(Runnable r){
+        this.r = r;
+    }
+
+    public void run(){
+        System.out.println("Thread 3 is running...");
+    }
+}
 class Main_Runnable1 {
     public static void main(String[] args) {
         System.out.println("Main thread is running...");
         Work w = new Work();
         Thread t = new Thread(w);
 
+        Work2 w2 = new Work2();
+        Thread t3 = new Thread(w2);
+
         Task t1 = new Task();
         Thread t2 = new Thread(t1);
 
         //w.sum();
+        
         t.start();
+        t3.start();
+
+        //t2.start();
 
         System.out.println("Main thread is running...");
         System.out.println("Main thread is running...");
