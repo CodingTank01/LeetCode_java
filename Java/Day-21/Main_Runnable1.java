51f1c6ed6e1ea81d8ac3b29de974a5aacf32c0c2
Aravind <aravindrk651@gmail.com>
2026-04-18 13:52:06 +0530

    PostLucnchNo.?

--- Java/Day-21/Main_Runnable1.java
+++ Java/Day-21/Main_Runnable1.java
@@ -1,9 +1,21 @@
-class Work implements Runnable {
+class Task implements Runnable {
     public void run(){
-        System.out.println("Thread 1 is running...");
+        System.out.println("Thread 2 is running...");
     }
-    public void run(int a){
-        System.out.println("Thread "+a+" is running...");
+}
+
+class Pending{
+    public void sum(){
+        System.out.println("Sum");
+    }
+}
+
+class Work extends Pending implements Runnable {
+    Pending p;
+    public void run(){
+        p = new Pending();
+        p.sum();
+        System.out.println("Thread 1 is running...");
     }
 }
 
@@ -12,14 +24,18 @@ class Main_Runnable1 {
         System.out.println("Main thread is running...");
         Work w = new Work();
         Thread t = new Thread(w);
+
+        Task t1 = new Task();
+        Thread t2 = new Thread(t1);
+
+        //w.sum();
         t.start();
 
-        //t.run(9);
-
         System.out.println("Main thread is running...");
         System.out.println("Main thread is running...");
         System.out.println("Main thread is running...");
         System.out.println("Main thread is running...");
+        t2.start();
         System.out.println("Main thread is running...");
         System.out.println("Main thread is running...");
         System.out.println("Main thread is running...");
